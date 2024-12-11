8fecc76cb87a54779d124378347a1c561479463d
MK <skdfw1212@naver.com>
2024-12-11 21:38:36 +0900

    a

--- Application/Actor.cpp
+++ Application/Actor.cpp
@@ -25,25 +25,26 @@ bool Actor::Initialize(IT_Renderer* renderer)
 
     m_cube = GeometryGenerator::MakeCube();
 
-    //for (uint32 i = 0; i < m_cube->numMeshes; i++)
-    //{
-    //    for (uint32 j = 0; j < m_cube->meshes[i].numVertices; j++)
-    //    {
-    //        printf("%f %f %f\n", m_cube->meshes[i].vertices[j].position.x, m_cube->meshes[i].vertices[j].position.y, m_cube->meshes[i].vertices[j].position.z);
-    //    }
-    //}
+    const wchar_t* basePath = L"../../Assets/";
+    const wchar_t* textureFilenames[] = {
+        L"tex_00.dds",
+        L"tex_01.dds",
+        L"tex_02.dds",
+        L"tex_03.dds",
+        L"tex_04.dds",
+        L"tex_05.dds",
+    };
 
-    //for (uint32 i = 0; i < m_cube->numMeshes; i++)
-    //{
-    //    for (uint32 j = 0; j < m_cube->meshes[i].numIndices; j++)
-    //    {
-    //        printf("%d ", m_cube->meshes[i].indices[j]);
-    //    }
-    //    printf("\n");
-    //}
+    for (uint32 i = 0; i < m_cube->numMeshes; i++)
+    {
+        wchar_t filePath[256] = {};
+        wcscat_s(filePath, basePath);
+        wcscat_s(filePath, textureFilenames[i]);
+
+        wcscpy_s(m_cube->meshes[i].textureFileaname, 256, filePath);
+    }
 
     m_meshObj = m_renderer->CreateMeshObject();
-    m_texture0 = m_renderer->CreateTextureFromFile(L"../../Assets/WoodCrate01.dds");
     m_meshObj->CreateMeshBuffers(m_cube);
     m_meshObj->SetTransform(m_transform);
 
@@ -56,7 +57,6 @@ void Actor::CleanUp()
     {
         m_meshObj->Release();
     }
-    m_renderer->DestroyTexture(m_texture0);
     GeometryGenerator::DestroyGeometry(m_cube);
 }
 
