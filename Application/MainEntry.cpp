4111c33888e48ef4a4b93f966d8cfd3db4ffe06f
MK <skdfw1212@naver.com>
2024-12-12 21:54:30 +0900

    hash table

--- Application/MainEntry.cpp
+++ Application/MainEntry.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "Application.h"
+#include "HashTable.h"
 
 extern "C" { __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001; }
 // D3D12 Agility SDK Runtime
@@ -26,6 +27,41 @@ int main(int argc, char* argv[])
 	{
 		return -1;
 	}
+
+	HashTable* hashTable = HT_CreateHashTable(10);
+	
+	HT_Insert(hashTable, (void*)L"tex01.dds", (void*)L"01");
+	HT_Insert(hashTable, (void*)L"tex01.dds", (void*)L"05");
+	HT_Insert(hashTable, (void*)L"tex02.dds", (void*)L"02");
+	HT_Insert(hashTable, (void*)L"tex03.dds", (void*)L"03");
+	HT_Insert(hashTable, (void*)L"tex04.dds", (void*)L"04");
+	HT_Insert(hashTable, (void*)L"tex05.dds", (void*)L"05");
+	HT_Insert(hashTable, (void*)L"tex06.dds", (void*)L"06");
+	HT_Insert(hashTable, (void*)L"tex07.dds", (void*)L"07");
+
+	wchar_t* value = (wchar_t*)HT_Find(hashTable, (void*)L"tex01.dds");
+	wprintf_s(L"%s\n", value);
+	value = (wchar_t*)HT_Find(hashTable, (void*)L"tex02.dds");
+	wprintf_s(L"%s\n", value);
+	value = (wchar_t*)HT_Find(hashTable, (void*)L"tex03.dds");
+	wprintf_s(L"%s\n", value);
+	value = (wchar_t*)HT_Find(hashTable, (void*)L"tex04.dds");
+	wprintf_s(L"%s\n", value);
+	value = (wchar_t*)HT_Find(hashTable, (void*)L"tex05.dds");
+	wprintf_s(L"%s\n", value);
+	value = (wchar_t*)HT_Find(hashTable, (void*)L"tex06.dds");
+	wprintf_s(L"%s\n", value);
+
+	value = (wchar_t*)HT_Find(hashTable, (void*)L"tex02.dds");
+	wprintf_s(L"%s\n", value);
+
+	Bucket* bucket = HT_Delete(hashTable, (void*)L"tex02.dds");
+	wprintf_s(L"%s\n", (wchar_t*)bucket->value);
+	HT_DestroyBucket(bucket);
+
+	HT_DestroyHashTable(hashTable);
+
+
 
 	int32 exitCode = app->RunApplication(); // success return code : 999
 	app->CleanUp();
